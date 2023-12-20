707de4469eade956a8c103a860b1e8b5146477bb
Jonah <kayiwa.jonah@student.moringaschool.com>
2023-12-21 02:33:42 +0300

    ##MATHS.JS

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+//MULTIPLY//
+num1 = 31  
+num2 = 2   
+multiply = (num1 * num2)
+print(multiply)
+
+
+//RANDOM //
+import (random)
+{
+random_number = random.randint(1, 100)  
+print(random_number)
+}
+
+//REMAINDER 4//
+num3 = 14  
+num4 = 5  
+mod = (num3 % num4)
+print(mod)
+
+
+//MAXIMU 20//
+numbers_set = [12, 14, 16, 18, 20] 
+max_value = max(numbers_set)
+print(max_value)
